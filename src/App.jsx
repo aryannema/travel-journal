3aa73ac84cdedc7280497cf418fd1d058dc6183f
aryannema <aaryan.techy@gmail.com>
2026-02-23 21:14:21 +0530

    added japan travel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import Header from "./components/Header";
+import Entry from "./components/Entry";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="container">
+        <Entry />
+      </main>
     </>
   );
 }
